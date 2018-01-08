19e63078ef551adae47b5d2e613f79a132b64668
Jtech123 <d181761@edu.rocwb.nl>
2018-01-08 16:49:36 +0100

    Initial commit

---
+++ Encryption2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Encryption2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.stringbox = new System.Windows.Forms.TextBox();
+            this.encryptedString = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.encryptedBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.encryptBtn.Location = new System.Drawing.Point(12, 218);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(114, 23);
+            this.encryptBtn.TabIndex = 0;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.decryptBtn.Location = new System.Drawing.Point(154, 218);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(116, 23);
+            this.decryptBtn.TabIndex = 1;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // stringbox
+            // 
+            this.stringbox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stringbox.Location = new System.Drawing.Point(12, 12);
+            this.stringbox.Name = "stringbox";
+            this.stringbox.Size = new System.Drawing.Size(258, 22);
+            this.stringbox.TabIndex = 0;
+            // 
+            // encryptedString
+            // 
+            this.encryptedString.AutoSize = true;
+            this.encryptedString.Location = new System.Drawing.Point(12, 82);
+            this.encryptedString.Name = "encryptedString";
+            this.encryptedString.Size = new System.Drawing.Size(60, 17);
+            this.encryptedString.TabIndex = 5;
+            this.encryptedString.Text = "STRING";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Location = new System.Drawing.Point(12, 128);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(60, 17);
+            this.Result.TabIndex = 6;
+            this.Result.Text = "STRING";
+            // 
+            // encryptedBox
+            // 
+            this.encryptedBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.encryptedBox.Location = new System.Drawing.Point(12, 40);
+            this.encryptedBox.Name = "encryptedBox";
+            this.encryptedBox.Size = new System.Drawing.Size(258, 22);
+            this.encryptedBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.encryptedBox);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.encryptedString);
+            this.Controls.Add(this.stringbox);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encryptBtn;
+        private System.Windows.Forms.Button decryptBtn;
+        private System.Windows.Forms.TextBox stringbox;
+        private System.Windows.Forms.Label encryptedString;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox encryptedBox;
+    }
+}
+
---
+++ Encryption2/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using CryptSharp;
+using System.Threading;
+
+namespace Encryption2
+{
+    public partial class Form1 : Form
+    {
+        private string original;
+        DateTime starttime;
+        DateTime endtime;
+
+        private string encrypted;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            original = stringbox.Text;
+            stringbox.Text = "";
+            encrypted = CryptSharp.BlowfishCrypter.Blowfish.Crypt(original);
+            encryptedString.Text = encrypted;
+            encryptedBox.Text = encrypted;
+        }
+
+        private void decryptBtn_Click(object sender, EventArgs e)
+        {
+            /*bool result = false;
+            byte[] test = new byte[255];
+            char[] chars = new char[255];
+            for (int asc = 0; asc < 255; asc++)
+            {
+                test[0] = Convert.ToByte(asc);
+                string check = System.Text.ASCIIEncoding.ASCII.GetString(test);
+
+                result = Sha256Crypter.CheckPassword(check, encrypted);
+                if (result)
+                {
+                    MessageBox.Show(result.ToString() + " Answer = " + check);
+                    break;
+                }
+            }*/
+            starttime = DateTime.Now;
+            Decrypt(0, 32);
+            //Handler();
+        } 
+        byte[] combination = new byte[10];
+        private void Decrypt(int characters, byte bit, int last = 0)
+        {
+            combination = new byte[255];
+            bool result = false;
+            string check = "";
+            while (!result)
+                for (int i = 0; i <= characters; i++)
+                {
+                    for (int j = 32; j < 137; j++)
+                    {
+                        combination[i] = Convert.ToByte(j);
+                        /*for (int k = 32; k < 137; k++)
+                        {
+                            combination[1] = Convert.ToByte(k);*/
+                        check = System.Text.ASCIIEncoding.ASCII.GetString(combination);
+                        result = BlowfishCrypter.CheckPassword(check, encrypted);
+                        if (result)
+                        {
+                            break;
+                        }
+                        //}
+                    }
+
+                }
+            if (result)
+            {
+                endtime = DateTime.Now;
+                MessageBox.Show("Started at: " + starttime.ToString() + " Ended at: " + endtime.ToString());
+                MessageBox.Show("Decrypted pass: " + check);
+            }
+            else
+            {
+                if (bit >= 137)
+                {
+                    if (characters >= last)
+                    {
+                        characters = 0;
+                        last++;
+                    }
+                    else
+                        characters++;
+                    bit = 32;
+                    //Result.Text = "Done with character nr " + characters.ToString();
+                    MessageBox.Show("Done with character nr " + characters.ToString());
+                    Decrypt(characters, bit, last);
+                }
+                else
+                {
+                    bit++;
+                    Decrypt(characters, bit);
+                }
+            }
+        }
+
+
+        private void Handler()
+        {
+            bool done = false;
+            string check = "";
+            while (!done)
+            {
+                for (int i = 0; i < 255; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        for (int k = 32; k < 137; k++)
+                        {
+                            DecryptNew(j, (byte)k);
+                            check = Encoding.ASCII.GetString(combination);
+                            done = Crypter.CheckPassword(check, encrypted);
+                            if (done)
+                            {
+                                //MessageBox.Show("Result");
+                                endtime = DateTime.Now;
+                                MessageBox.Show("Started at: " + starttime.ToString() + " Ended at: " + endtime.ToString());
+                                MessageBox.Show("Decrypted pass: " + check);
+                                return;
+                            }
+                        }
+                        if (done)
+                        {
+                            //MessageBox.Show("Result");
+                            endtime = DateTime.Now;
+                            MessageBox.Show("Started at: " + starttime.ToString() + " Ended at: " + endtime.ToString());
+                            MessageBox.Show("Decrypted pass: " + check);
+                            return;
+
+                        }
+                        else
+                        {
+                            //MessageBox.Show("It is longer than " + (j + 1) + " characters...");
+                        }
+                    }
+                }
+                if (done)
+                {
+                    //MessageBox.Show("Result");
+                    endtime = DateTime.Now;
+                    MessageBox.Show("Started at: " + starttime.ToString() + " Ended at: " + endtime.ToString());
+                    MessageBox.Show("Decrypted pass: " + check);
+                    return;
+                }
+            }
+            if (done)
+            {
+                endtime = DateTime.Now;
+                MessageBox.Show("Started at: " + starttime.ToString() + " Ended at: " + endtime.ToString());
+                MessageBox.Show("Decrypted pass: " + check);
+            }
+        }
+        private void DecryptNew (int chars, byte bit)
+        {
+
+            combination[chars] = bit;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stringbox.Focus();
+        }
+    }
+}
